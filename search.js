c0bf42066968a2c41eb31d20d6f701fb1ceac853
Sofia Zaid <sofia.zaid@alten.se>
2019-01-27 16:49:51 +0100

    WIP

--- search.js
+++ search.js
@@ -1,5 +1,39 @@
+/*
+  Clears the search result container from existing elements.
+  Loops through all ancestors of search result list and
+  deletes as long as there are list items.
+*/
+function clearSearchResultArea() {
+  let elementsToRemove = document.getElementById("searchResultList");
+
+  while (elementsToRemove.firstChild) {
+    elementsToRemove.removeChild(elementsToRemove.firstChild);
+  }
+}
+
+function addResult(resultList, answer, sourceText) {
+  let listItem = document.createElement("li");
+  let resultText = document.createTextNode(answer);
+  listItem.appendChild(resultText);
+  if (null !== sourceText && sourceText !== undefined) {
+    let resultSource = document.createTextNode(sourceText);
+
+    listItem.appendChild(resultSource);
+  }
+  listItem.classList.add("folded");
+  listItem.addEventListener("click", evt => {
+    listItem.classList.toggle("folded");
+  });
+  resultList.appendChild(listItem);
+}
+
 let form = document.getElementById("search-form");
 
+/*
+  Defines eventhandler for submit action for search-"form".
+  API-call is defined in eventhandler and input question
+  is sent as a POST-request in body.
+*/
 form.addEventListener("submit", () => {
   let url =
     "https://telosgreen.azurewebsites.net/qnamaker/knowledgebases/ce641050-750c-4279-a3ca-7e649a5b88b8/generateAnswer";
@@ -8,40 +42,29 @@ form.addEventListener("submit", () => {
 
   let body = JSON.stringify({ question: data });
 
+  //Call method to clear search result area before showing new search
+  //result.
+  clearSearchResultArea();
+
   const searchResult = async () => {
     const response = await fetch(url, {
       method: "POST",
       body: body,
       headers: {
         "Content-Type": "application/json",
-        "Authorization": "EndpointKey d1686420-c822-421a-ac16-5055906050d1"
+        Authorization: "EndpointKey d1686420-c822-421a-ac16-5055906050d1"
       }
     });
     const myJson = await response.json();
-   //console.log((myJson));
+    let resultList = document.getElementById("searchResultList");
 
-    //let obj = JSON.parse(myJson);
-    //console.log(obj);
-    //let answers = new Array(myJson.answers.length);
-
-      let answer = myJson.answers[0].answer;
-      console.log(answer);
+    /*
+      Loop through all elements in response' answers list, 
+      add searchitems to result list.
+    */
+    for (let i in myJson.answers) {
+      addResult(resultList, myJson.answers[i].answer, myJson.answers[i].source);
+    }
   };
-  
   searchResult();
-
-  const searchBlockContainer = document.getElementById('id="search-form3-1v"');
-  const searchResultContainer = document.createElement('article');
-  searchResultContainer.setAttribute('class', 'container');
-  searchResultContainer.setAttribute('id', 'searchResultArea');
-  searchBlockContainer.appendChild(searchResultContainer);
-
-  // let searchR = searchResult();
-  // let result = JSON.stringify(searchR);
-
-  // obj = 
-
-  //console.log(result);
-
 });
-
